f3e7331e2657f3261cb8ab7cad7e4cfdb7f4ddb0
daraem <daraemxd@gmail.com>
2024-10-22 15:34:37 +0100

    .

--- script.js
+++ script.js
@@ -5,39 +5,87 @@ const steamDiv = document.getElementById("steamResult")
 const entryPrice = document.getElementById("entryPrice")
 const sellPrice = document.getElementById("sellPrice")
 
-const taxSitesRef = ["csFloat", "skinPort", "steam"]
-let taxSites = {
-    "csFloat": {
+let taxSites = [
+    {
+        name: "csFloat",
         div: csFloatDiv,
-        tax: 2
+        fee: 2
     },
-    "skinPort": {
-        "div": skinportDiv,
-        "tax": 12
+    {
+        name: "skinPort",
+        div: skinportDiv,
+        fee: 12
     },
-    "steam": {
-        "div": steamDiv,
-        "tax": 15
+    {
+        name: "steam",
+        div: steamDiv,
+        fee: 15
     }
-} 
+]
 
 function feeCalc(entryPrice, sellPrice, fee) {
     return [sellPrice - (sellPrice * fee), (sellPrice - (sellPrice * fee) - entryPrice).toFixed(2), true ? ((sellPrice - (sellPrice * fee)) > entryPrice) : false]
 }
 
 function setSellPrice() {
-    taxSitesRef.forEach((i, k) => {
-        taxSites[taxSitesRef[k]].div.innerText = `Sell: ${feeCalc(entryPrice.value, sellPrice.value, (taxSites[taxSitesRef[k]].tax / 100))[0]} | Profit: ${feeCalc(entryPrice.value, sellPrice.value, (taxSites[taxSitesRef[k]].tax / 100))[1]}`
-        taxSites[taxSitesRef[k]].div.style.color = feeCalc(entryPrice.value, sellPrice.value, (taxSites[taxSitesRef[k]].tax / 100))[2] ? "green" : "red"
+    taxSites.forEach((i, k) => {
+        console.log(taxSites[k])
+        taxSites[k].div.innerHTML = `Sell: ${feeCalc(entryPrice.value, sellPrice.value, (taxSites[k].fee / 100))[0]} | Profit: ${feeCalc(entryPrice.value, sellPrice.value, (taxSites[k].fee / 100))[1]}`
+        taxSites[k].div.style.color = feeCalc(entryPrice.value, sellPrice.value, (taxSites[k].fee / 100))[2] ? "green" : "red"
     })
 }
 
-function addSite() {
+function addSite(name, fee) {
+    let siteDiv = document.createElement("div")
+    let siteName = document.createElement("p")
+    let siteResult = document.createElement("p")
+    let deleteBut = document.createElement("button")
+    deleteBut.innerHTML = "Delete"
+    siteResult.setAttribute("id", name)
+    siteName.innerHTML = name
+
+    siteDiv.append(siteName)
+    siteDiv.append(siteResult)
+    siteDiv.append(deleteBut)
+    document.body.append(siteDiv)
+
+    setTimeout(() => {
+        taxSites.push({
+            "name": name,
+            "div": document.getElementById(name),
+            "fee": Number(fee)
+        })
+    }, 100)
+
+    deleteBut.addEventListener("click", () => {
+        siteName.remove()
+        siteResult.remove()
+        deleteBut.remove()
+        siteDiv.remove()
+    })
+}
+
+function submitSite() {
     let inputName = document.createElement("input")
     let inputTax = document.createElement("input")
+    let submitButton = document.createElement("Button")
+    submitButton.innerHTML = "Submit"
+    
     inputName.setAttribute("placeholder", "name")
+    inputTax.setAttribute("placeholder", "fee")
+
     document.getElementById("newSite").append(inputName)
     document.getElementById("newSite").append(inputTax)
+    document.getElementById("newSite").append(submitButton)
+
+    submitButton.addEventListener("click", () => {
+        addSite(inputName.value, inputTax.value)
+        setTimeout(() => {
+            inputName.remove()
+            inputTax.remove()
+            submitButton.remove()
+        }, 100)
+    })
 }
 
 "keypress mouseup".split(" ").forEach((e) => {
